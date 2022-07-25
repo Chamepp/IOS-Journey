c0fdce9c5a2e54d64769fb11dc6eb6fdc2ed1ea2
System Administrator <ashkanebtekari@gmail.com>
2022-07-25 09:53:28 +0430

    added project two

---
+++ core-skills/project-two/project-two/project_twoApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_twoApp.swift
+//  project-two
+//
+//  Created by Ashkan Ebtekari on 7/25/22.
+//
+
+import SwiftUI
+
+@main
+struct project_twoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
