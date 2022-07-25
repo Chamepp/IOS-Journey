eafe931e22e74bfe35ff68fe5e34bb9cfeb4b734
System Administrator <ashkanebtekari@gmail.com>
2022-07-25 09:57:33 +0430

    adding my created hello world app

---
+++ core-skills/HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Ashkan Ebtekari on 7/25/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
