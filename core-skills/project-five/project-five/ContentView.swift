96f665a96dbe09ba04ec10e245833c515681d530
System Administrator <ashkanebtekari@gmail.com>
2022-07-31 17:19:55 +0430

    created project-five

---
+++ core-skills/project-five/project-five/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  project-five
+//
+//  Created by Ashkan Ebtekari on 7/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ core-skills/project-five/project-five/project_fiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_fiveApp.swift
+//  project-five
+//
+//  Created by Ashkan Ebtekari on 7/31/22.
+//
+
+import SwiftUI
+
+@main
+struct project_fiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
