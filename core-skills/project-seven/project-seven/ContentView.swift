db9f705be1e8270b7036b422d5273272d8669ee3
System Administrator <ashkanebtekari@gmail.com>
2022-08-13 20:38:02 +0430

    completed project seven challenges

--- core-skills/project-seven/project-seven/ContentView.swift
+++ core-skills/project-seven/project-seven/ContentView.swift
@@ -13,55 +13,101 @@ struct ContentView: View {
     @State private var root_word = ""
     @State private var new_word = ""
     
+    @State private var score = 0
+    @State private var high_score = 0
+    
     @State private var error_title = ""
     @State private var error_message = ""
     @State private var showing_error = false
     
+    @State private var showing_new_word = false
+    
     var body: some View {
         NavigationView {
-            List {
-                Section {
-                    TextField("What's Your Word", text: $new_word)
-                        .autocapitalization(.none)
+            VStack {
+                VStack(alignment: .leading) {
+                    Text(root_word)
+                        .font(.system(size: 45, weight: .regular, design: .rounded))
+                    Text("Score: \(score)     Highscore: \(high_score)")
+                        .font(.system(size: 20, weight: .light, design: .rounded))
                 }
-                Section {
-                    ForEach(used_words, id: \.self) { word in
-                        HStack {
-                            Image(systemName: "\(word.count).circle.fill")
-                            Text(word)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal, 20)
+                List {
+                    Section {
+                        TextField("What's Your Word", text: $new_word)
+                            .autocapitalization(.none)
+                    }
+                    Section {
+                        ForEach(used_words, id: \.self) { word in
+                            HStack {
+                                Image(systemName: "\(word.count).circle.fill")
+                                Text(word)
+                            }
                         }
                     }
                 }
-            }
-            .navigationTitle(root_word)
-            .onSubmit(add_new_word)
-            .onAppear(perform: start_game)
-            .alert(error_title, isPresented: $showing_error) {
-                Button("Ok", role: .cancel) {}
-            } message: {
-                Text(error_message)
+                .listStyle(.plain)
+                .toolbar {
+                    Button {
+                        showing_new_word = true
+                    } label: {
+                        Text("New Word")
+                    }
+                }
+                .onSubmit(add_new_word)
+                .onAppear(perform: start_game)
+                .alert(error_title, isPresented: $showing_error) {
+                    Button("Ok", role: .cancel) {}
+                } message: {
+                    Text(error_message)
+                }
+                .alert("New Word", isPresented: $showing_new_word) {
+                    Button("Ok", role: .destructive) {
+                        start_game()
+                        score -= 2
+                    }
+                    Button("Cancel", role: .cancel, action: start_game)
+                } message: {
+                    Text("New word costs 2 points")
+                }
             }
         }
     }
     func add_new_word() {
         let answer = new_word.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
-        guard answer.count > 0 else { return }
         
+        guard isMore(word: answer) else {
+            word_error(title: "Word should be more than two", message: "Select a bigger word")
+            return
+        }
+        guard answer != root_word else {
+            word_error(title: "You just repeated the word", message: "Try a diffrent structure")
+            return
+        }
         guard isOriginal(word: answer) else {
-            word_error(title: "Word have already been used", message: "Select a more original word")
+            score -= 1
+            word_error(title: "You Losed a Point !", message: "Word have already been used")
             return
         }
         guard isPossible(word: answer) else {
-            word_error(title: "Word is not possible", message: "You cant spell that word from \(root_word)!")
+            score -= 1
+            word_error(title: "You Losed a Point", message: "Word is not possible")
             return
         }
         guard isReal(word: answer) else {
-            word_error(title: "Word not recognized", message: "You can just make it up!")
+            score -= 1
+            word_error(title: "You Losed a Point", message: "You can't just make it up!")
             return
         }
         
         withAnimation {
             used_words.insert(answer, at: 0)
+        }
+        
+        score += 1
+        if score > high_score {
+            high_score = score
         }
         new_word = ""
     }
@@ -97,6 +143,13 @@ struct ContentView: View {
         let misspelled_range = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
         
         return misspelled_range.location == NSNotFound
+    }
+    func isMore(word: String) -> Bool {
+        if new_word.count > 2 {
+            return true
+        } else {
+            return false
+        }
     }
     func word_error(title: String, message: String) {
         error_title = title
