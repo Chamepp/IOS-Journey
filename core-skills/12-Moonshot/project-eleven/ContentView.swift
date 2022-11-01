b083746fa5480ec54cd4f7ae1a6142cdf6e19809
System Administrator <ashkanebtekari@gmail.com>
2022-11-01 12:01:16 +0330

    Starting Moonshot

---
+++ core-skills/12-Moonshot/project-eleven/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  project-eleven
+//
+//  Created by Ashkan Ebtekari on 11/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ core-skills/12-Moonshot/project-eleven/project_elevenApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_elevenApp.swift
+//  project-eleven
+//
+//  Created by Ashkan Ebtekari on 11/1/22.
+//
+
+import SwiftUI
+
+@main
+struct project_elevenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
