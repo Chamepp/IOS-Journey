ffba02bc1595a039f069fe95616f89c4bba002c2
System Administrator <ashkanebtekari@gmail.com>
2022-11-06 18:37:09 +0330

    started Moonshot

---
+++ core-skills/12-Moonshot/project-eleven/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  project-eleven
+//
+//  Created by Ashkan Ebtekari on 11/6/22.
+//
+
+import Foundation
+
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ core-skills/12-Moonshot/project-eleven/Bundle-Decodable.swift
@@ -0,0 +1,8 @@
+//
+//  Bundle-Decodable.swift
+//  project-eleven
+//
+//  Created by Ashkan Ebtekari on 11/6/22.
+//
+
+import Foundation
