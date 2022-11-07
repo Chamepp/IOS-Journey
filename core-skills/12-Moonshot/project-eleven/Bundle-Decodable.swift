23d84b1d82dda220b36231fc2aa094bfce89c17d
System Administrator <ashkanebtekari@gmail.com>
2022-11-07 20:38:11 +0330

    added assets

--- core-skills/12-Moonshot/project-eleven/Bundle-Decodable.swift
+++ core-skills/12-Moonshot/project-eleven/Bundle-Decodable.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+extension Bundle {
+    func decode(_ filename: String) -> [String: Astronaut] {
+        guard let url = self.url(forResource: filename, withExtension: nil) else {
+            fatalError("Unable to locate \(filename) in bundle")
+        }
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(filename) from bundle")
+        }
+        
+        let decoder = JSONDecoder()
+        guard let loaded = try? decoder.decode([String: Astronaut].self, from: data) else {
+            fatalError("UNable to decode file from bundle")
+        }
+        
+        return loaded
+    }
+}
